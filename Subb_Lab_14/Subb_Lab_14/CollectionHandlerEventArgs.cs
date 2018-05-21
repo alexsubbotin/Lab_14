4066cd4fcc10a5ce15a400a8390e63791634ece2
Alexey Subbotin <subbotin910@gmail.com>
2018-05-21 15:32:35 +0500

    event handler class added

---
+++ Subb_Lab_14/Subb_Lab_14/CollectionHandlerEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subb_Lab_14
+{
+    class CollectionHandlerEventArgs
+    {
+        // Collection name.
+        public string Name { get; set; }
+
+        // Type of change.
+        public string ChangeType { get; set; }
+    }
+}
