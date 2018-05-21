b452d7ce0a70cd3f3fcc5249794cf1e8944f5e87
Alexey Subbotin <subbotin910@gmail.com>
2018-05-21 15:53:56 +0500

    MyNewDictionary: indexer, remove, name added

--- Subb_Lab_14/Subb_Lab_14/MyNewDictionary.cs
+++ Subb_Lab_14/Subb_Lab_14/MyNewDictionary.cs
@@ -8,6 +8,42 @@ namespace Subb_Lab_14
 {
     class MyNewDictionary<K, T>: MyDictionary<K, T> 
     {
+        // Delegate for events.
+        public delegate void CollectionHandler(object source, CollectionHandlerEventArgs args);
 
+        // Collection name.
+        public string Name { get; set; }
+
+        // Remove from the j position.
+        public bool Remove(int j)
+        {
+            if (j > Capacity)
+                return false;
+            else
+            {
+                base.Remove(Table[j]);
+                return true;
+            }
+        }
+
+        // Indexer.
+        public DicPoint<K,T> this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Capacity)
+                    return Table[index];
+                else
+                    throw new IndexOutOfRangeException();
+            }
+
+            set
+            {
+                if (index >= 0 && index < Capacity)
+                    Table[index] = value;
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
     }
 }
