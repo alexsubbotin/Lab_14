4da74c0e90d1c62f1a02185b2d991b30b8945117
Alexey Subbotin <subbotin910@gmail.com>
2018-05-21 15:37:11 +0500

    event handler class developed

--- Subb_Lab_14/Subb_Lab_14/CollectionHandlerEventArgs.cs
+++ Subb_Lab_14/Subb_Lab_14/CollectionHandlerEventArgs.cs
@@ -13,5 +13,22 @@ namespace Subb_Lab_14
 
         // Type of change.
         public string ChangeType { get; set; }
+
+        // Link to the changed object.
+        public State Obj { get; set; }
+
+        // Constructor.
+        public CollectionHandlerEventArgs(string name, string changeType, State obj)
+        {
+            this.Name = name;
+            this.ChangeType = changeType;
+            this.Obj = obj;
+        }
+
+        // To string method.
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nType of change: " + ChangeType + "\n Object: " + Obj.ToString();
+        }
     }
 }
