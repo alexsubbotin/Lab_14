acd89b8d3e61128eb9f3109c75e4c61c1c3547c7
Alexey Subbotin <subbotin910@gmail.com>
2018-05-21 16:26:51 +0500

    JournalEntry class added

---
+++ Subb_Lab_14/Subb_Lab_14/JournalEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subb_Lab_14
+{
+    class JournalEntry
+    {
+        // Name of the changed collection.
+        public string Name { get; set; }
+
+        // Type of change.
+        public string ChangeType { get; set; }
+
+        // Information about the object.
+        public string ObjInfo { get; set; }
+
+        // Constructor.
+        public JournalEntry(string name, string changeType, string objInfo)
+        {
+            this.Name = name;
+            this.ChangeType = changeType;
+            this.ObjInfo = objInfo;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nType of change: " + ChangeType + "\n Object: " + ObjInfo;
+        }
+    }
+}
