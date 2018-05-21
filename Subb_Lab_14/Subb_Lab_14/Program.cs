b789584edf93fd93aa66a109e906a8dfc57a851c
Alexey Subbotin <subbotin910@gmail.com>
2018-05-21 19:04:02 +0500

    start of demo

--- Subb_Lab_14/Subb_Lab_14/Program.cs
+++ Subb_Lab_14/Subb_Lab_14/Program.cs
@@ -10,6 +10,85 @@ namespace Subb_Lab_14
     {
         static void Main(string[] args)
         {
+            int choice;
+            do
+            {
+                Console.WriteLine(@"Choose one of the options:
+1. Work with MyDictionary
+2. Demonstrate the events
+3. Exit");
+
+                choice = MyDicDemo.ChoiceInput(3);
+
+                switch (choice)
+                {
+                    case 1:
+                        MyDicDemo.Demonstrate();
+                        break;
+                    case 2:
+                        EventsDemo();
+                        break;
+                }
+            } while (choice != 3);
+        }
+
+        public static void EventsDemo()
+        {
+            // The first collection.
+
+            MyNewDictionary<int, State> dic1 = new MyNewDictionary<int, State>();
+
+            MyDictionary<int, State> buf = new MyDictionary<int, State>(4);
+            MyDicDemo.FillDic(ref buf);
+
+            dic1 = new MyNewDictionary<int, State>(buf);
+
+            dic1.Name = "dic1";
+
+            // The second collection.
+            MyNewDictionary<int, State> dic2 = new MyNewDictionary<int, State>(4);
+
+            buf = new MyDictionary<int, State>(4);
+            MyDicDemo.FillDic(ref buf);
+
+            dic2 = new MyNewDictionary<int, State>(buf);
+
+            dic2.Name = "dic2";
+
+            // The first journal.
+            Journal journal1 = new Journal();
+
+            // The second journal.
+            Journal journal2 = new Journal();
+
+            // Subscription of the first journal.
+            dic1.CollectionCountChanged += new CollectionHandler(journal1.CollectionCountChanged);
+            dic1.CollectionReferenceChanged += new CollectionHandler(journal1.CollectionReferenceChanged);
+
+            // Subscription of the second journal.
+            dic1.CollectionReferenceChanged += new CollectionHandler(journal2.CollectionReferenceChanged);
+            dic2.CollectionReferenceChanged += new CollectionHandler(journal2.CollectionReferenceChanged);
+
+            // Generating a random state object.
+            State rnd = MyDicDemo.RandomState();
+
+            // Adding the elem to the 1st collection.
+            dic1.Add(rnd.Name.Length, rnd);
+            // Removing an object from the 1st collection.
+            dic1.Remove(0);
+            // Changing a value int the 1st collection.
+            dic1.Remove(rnd);
+            DicPoint<int, State> dicPointBuf = new DicPoint<int, State>(rnd.Name.Length, rnd);
+            dic1.Table[0] = dicPointBuf;
+
+            // Adding the elem to the 2nd collection.
+            dic1.Add(rnd.Name.Length, rnd);
+            // Removing an object from the 2nd collection.
+            dic1.Remove(0);
+            // Changing a value int the 2nd collection.
+            dic1.Remove(rnd);
+            dicPointBuf = new DicPoint<int, State>(rnd.Name.Length, rnd);
+            dic1.Table[0] = dicPointBuf;
         }
     }
 }
