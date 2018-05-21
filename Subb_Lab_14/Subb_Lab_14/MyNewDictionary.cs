04374c0de71916df0f887fb90d9ee0c36a81e8e1
Alexey Subbotin <subbotin910@gmail.com>
2018-05-21 15:32:24 +0500

    MyNewDictionary class added

---
+++ Subb_Lab_14/Subb_Lab_14/MyNewDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subb_Lab_14
+{
+    class MyNewDictionary<K, T>: MyDictionary<K, T> 
+    {
+
+    }
+}
